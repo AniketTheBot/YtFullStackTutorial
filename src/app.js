05f6cd0277f748ad24fb929fa9201e1f01f648ab
AniketTheBot <aniloona@gmail.com>
2025-07-24 02:31:10 +0530

    Add Cloudinary integration and multer middleware; implement user registration route

--- src/app.js
+++ src/app.js
@@ -8,22 +8,25 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(
   express.json({
     limit: "16kb",
   })
 );
-
 app.use(
   express.urlencoded({
     extended: true,
     limit: "16kb",
   })
 );
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+// routes
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Click 'View API Keys' above to copy your cloud name
+  api_key: process.env.CLOUDINARY_API_KEY, // Click 'View API Keys' above to copy your API key
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File is uploaded on cloudinary, ", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    // This removes the locally saved temporary file as the upload operation failes
+
+  }
+};
+
+

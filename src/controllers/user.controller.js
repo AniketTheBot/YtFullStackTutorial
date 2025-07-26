85e4b50c4d8463fb70e69f2c448f5d22938f1213
AniketTheBot <aniloona@gmail.com>
2025-07-27 01:54:15 +0530

    Fix refresh token validation logic to throw an error for missing tokens

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -197,7 +197,7 @@ export const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized request");
   }
 
